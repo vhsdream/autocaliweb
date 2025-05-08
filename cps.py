aac52ed51ad04c9828c4182d8c6a16ce032fdd53
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-05-08 18:23:52 +0200

    Remove console window hiding function for windows
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- cps.py
+++ cps.py
@@ -27,24 +27,5 @@ sys.path.insert(0, path)
 
 from cps.main import main
 
-
-def hide_console_windows():
-    import ctypes
-
-    kernel32 = ctypes.WinDLL('kernel32')
-    user32 = ctypes.WinDLL('user32')
-
-    SW_HIDE = 0
-
-    hWnd = kernel32.GetConsoleWindow()
-    if hWnd:
-        user32.ShowWindow(hWnd, SW_HIDE)
-
-
 if __name__ == '__main__':
-    if os.name == "nt":
-        hide_console_windows()
     main()
-
-
-
