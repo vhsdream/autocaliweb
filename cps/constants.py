e3534730528b5b420ea02179f71b38b3522d53a7
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-07-29 21:17:01 +0200

    v0.7.5
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- cps/constants.py
+++ cps/constants.py
@@ -170,7 +170,7 @@ BookMeta = namedtuple('BookMeta', 'file_path, extension, title, author, cover, d
                                   'series_id, languages, publisher, pubdate, identifiers')
 
 # python build process likes to have x.y.zbw -> b for beta and w a counting number
-STABLE_VERSION =  'v0.7.4'
+STABLE_VERSION =  'v0.7.5'
 
 USER_AGENT = f"Autocaliweb/{STABLE_VERSION}"
 
